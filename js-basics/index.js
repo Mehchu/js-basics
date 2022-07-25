08327bdfb2278bd323588768af2683ea2cab2387
Mehchu <dtmackenzie06@outlook.com>
2022-07-25 16:58:31 +0100

    Converted index.js into a glossary for JavaScript

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,3 +1,28 @@
+// Variable declaration
+let name = 'Bob'
+const PI = 3.141592
 
-let name = 'Daniel';
-console.log(name);
+// Making an object
+let person = {
+    name: 'Daniel',
+    age: 30
+};
+
+// Dot Notation
+person.name = 'John'
+
+// Bracket Notation
+person['name'] = 'Mary'
+
+// Initialise array
+let selectedColors = ['red', 'blue'];
+
+// Add elements to an array
+selectedColors[2] = 1
+
+// Functions
+function square(number) {
+    return number ** 2
+}
+
+console.log(square(2));
